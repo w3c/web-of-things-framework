448170b6214193459e06b31861bf9e5de5483000
Robert Horvick <robert.horvick@gmail.com>
2015-06-18 13:20:08 -0400

    Channel manager ideas

---
+++ channels/channel.js
@@ -0,0 +1,49 @@
+﻿
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+function Channel(uri) {
+    var self = this;
+
+    if (self.constructor === Channel) {
+        throw new Error('Channel is an abstract class');
+    }
+
+    EventEmitter.call(self);
+
+    self._uri = uri;
+}
+
+util.inherits(Channel, EventEmitter);
+
+Channel.prototype.uri = function() {
+    return self._uri;
+}
+
+Channel.prototype.connect = function(uri, succeed, fail) {
+    var self = this;
+
+    if (self.constructor === Channel) {
+        throw new Error('Channel::connect is an abstract method');
+    }
+}
+
+Channel.prototype.send = function(message) {
+    var self = this;
+
+    if (self.constructor === Channel) {
+        throw new Error('Channel::send is an abstract method');
+    }
+}
+
+Channel.prototype.messages = {
+    open: "open",
+    close: "close",
+    action: "action",
+    patch: "patch",
+    proxy: "proxy",
+    uri: "uri",
+    error: "error"
+}
+
+module.exports = Channel;
---
+++ channels/factory.js
@@ -0,0 +1,9 @@
+﻿
+var ChannelFactoryMap = require('./factorymap.js');
+var WebSocketChannelFactory = require('./ws/factory.js');
+
+
+var wsFactory = new WebSocketChannelFactory();
+ChannelFactoryMap.register(wsFactory.protocol(), wsFactory);
+
+module.exports = ChannelFactoryMap;
